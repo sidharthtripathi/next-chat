7183d46ae2f05c2a6de287ccddc7c75ab0f7a873
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2024-07-04 13:32:46 +0530

    fix middleware jwt error

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,27 @@
-import { cookies } from 'next/headers'
+import { cookies } from "next/headers";
 import { NextResponse, type NextRequest } from "next/server";
-import {jwtVerify} from 'jose'
-import { createSecretKey } from 'crypto';
-export async function middleware(req : NextRequest){
-    const token = cookies().get("token")
-    const requestHeaders = new Headers(req.headers)
+import { jwtVerify } from "jose";
+import { createSecretKey } from "crypto";
+export async function middleware(req: NextRequest) {
+  const token = cookies().get("token");
+  const requestHeaders = new Headers(req.headers);
 
-    if(token){ 
-        const {payload} = await jwtVerify(token.value,new TextEncoder().encode(process.env.JWT_SECRET as string))
-        
-        requestHeaders.set("username",payload.username as string)
-        requestHeaders.set("id",payload.id as string)
-        return NextResponse.next({
-            request : {
-                headers  : requestHeaders
-            }
-        })
+  if (token) {
+    try {
+      const { payload } = await jwtVerify(
+        token.value,
+        new TextEncoder().encode(process.env.JWT_SECRET as string)
+      );
+
+      requestHeaders.set("username", payload.username as string);
+      requestHeaders.set("id", payload.id as string);
+      return NextResponse.next({
+        request: {
+          headers: requestHeaders,
+        },
+      });
+    } catch (error) {
+      return NextResponse.next();
     }
-    
+  }
 }
-
-
